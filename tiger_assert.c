e53dfbe3cfb81bcf313fc2c237ecf08dae942044
root <root@localhost.localdomain>
2018-01-11 08:08:24 -0500

    word fix

--- tiger_assert.c
+++ tiger_assert.c
@@ -23,7 +23,7 @@ void tiger_assert_win(bool condition,char* fmt,...)
     if(condition)
         return;
     
-    fprintf(stderr,"[tiger_assertion <%s,%d> Asserion failed] ",assert_info.file,assert_info.line);
+    fprintf(stderr,"[tiger_assertion <%s,%d> Assertion failed] ",assert_info.file,assert_info.line);
     va_list args;
     va_start(args,fmt);
     vfprintf(stderr,fmt,args);
