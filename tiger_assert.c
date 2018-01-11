6778d797055f7120f5d64febf6fb7a4002c0f4c0
supersojo <suyanjun218@163.com>
2018-01-11 16:23:55 +0800

    new assert on windows platform support

--- tiger_assert.c
+++ tiger_assert.c
@@ -5,10 +5,25 @@
 
 namespace tiger{
 
+struct  AssertInfo  assert_info={0};
+
 void tiger_assert(bool condition,char* fmt,...)
 {
     if(condition)
         return;
+    va_list args;
+    va_start(args,fmt);
+    vfprintf(stderr,fmt,args);
+    fprintf(stderr,"\nAbort\n");
+    va_end(args);
+    exit(-1);
+}
+void tiger_assert_win(bool condition,char* fmt,...)
+{
+    if(condition)
+        return;
+    
+    fprintf(stderr,"[tiger_assertion <%s,%d> Asserion failed] ",assert_info.file,assert_info.line);
     va_list args;
     va_start(args,fmt);
     vfprintf(stderr,fmt,args);
--- tiger_assert.h
+++ tiger_assert.h
@@ -7,22 +7,42 @@
 
 #include "tiger_type.h"
 
-#define TIGER_ASSERT(condition,msg...) \
-do{ \
-    if(!(condition)) \
-    { \
-    fprintf(stderr,"[tiger_assertion: <%s,%d> Assertion<%s> failed] ",__FILE__,__LINE__,#condition); \
-    tiger::tiger_assert(condition,##msg); \
-    } \
-}while(0)
+
 namespace tiger{
 
+struct AssertInfo{
+    char* file;
+    s32 line;
+};
+
+extern struct AssertInfo assert_info;
+
 void tiger_assert(bool condition,char* fmt,...);
-
-
-
+void tiger_assert_win(bool condition,char* fmt,...);
 
 }//namespace tiger
+
+
+#ifdef _WIN32
+#   define TIGER_ASSERT \
+    do{ \
+        tiger::assert_info.file = __FILE__;\
+        tiger::assert_info.line = __LINE__;\
+    }while(0);tiger::tiger_assert_win
+
+#else //!_WIN32
+    
+#   define TIGER_ASSERT(condition,msg...) \
+    do{ \
+        if(!(condition)) \
+        { \
+        fprintf(stderr,"[tiger_assertion: <%s,%d> Assertion<%s> failed] ",__FILE__,__LINE__,#condition); \
+        tiger::tiger_assert(condition,##msg); \
+        } \
+    }while(0)
+        
+#endif // !_WIN32 
+
 
 
 
